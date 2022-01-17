e36062a3918e168554e9cc422ad329c3a0de7e64
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-17 16:11:50 +0700

    change host

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,14 +123,14 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        // baseURL: 'https://backend-market-tukang.herokuapp.com',
-        baseURL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080',
+        baseURL: 'https://backend-market-tukang.herokuapp.com',
+        // baseURL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080',
     },
     env: {
-        // API_BASE_URL: 'https://backend-market-tukang.herokuapp.com/api/v1',
-        // BASE_URL: 'https://backend-market-tukang.herokuapp.com'
-        API_BASE_URL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080/api/v1',
-        BASE_URL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080'
+        API_BASE_URL: 'https://backend-market-tukang.herokuapp.com/api/v1',
+        BASE_URL: 'https://backend-market-tukang.herokuapp.com'
+            // API_BASE_URL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080/api/v1',
+            // BASE_URL: 'http://ec2-18-142-122-133.ap-southeast-1.compute.amazonaws.com:8080'
     },
 
 
